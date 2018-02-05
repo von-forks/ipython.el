4d78fd288a888ce6b01395a6acc5d39d6f727c6d
Joe Bloggs <vapniks@yahoo.com>
2018-02-05 19:19:27 +0000

    Bugfixes to work with newer versions of ipython

--- ipython.el
+++ ipython.el
@@ -294,14 +294,14 @@ gets converted to:
 
 "
   (interactive "*r\n")
-  ;(message (format "###DEBUG s:%de:%d" start end))
+					;(message (format "###DEBUG s:%de:%d" start end))
   (save-excursion
     (save-match-data
       ;; replace ``In [3]: bla`` with ``>>> bla`` and
       ;;         ``... :   bla`` with ``...    bla``
       (goto-char start)
       (while (re-search-forward ipython-de-input-prompt-regexp end t)
-        ;(message "finding 1")
+					;(message "finding 1")
         (cond ((match-string 3)         ;continued
                (replace-match "... \\3" t nil))
               (t
@@ -312,7 +312,7 @@ gets converted to:
         (replace-match "" t nil)))))
 
 (defvar ipython-completion-command-string
-  "print ';'.join(__IP.Completer.all_completions('%s')) #PYTHON-MODE SILENT\n"
+  "print ';'.join(get_ipython().Completer.all_completions('%s')) #PYTHON-MODE SILENT\n"
   "The string send to ipython to query for all possible completions")
 
 
@@ -327,7 +327,7 @@ in the current *Python* session."
       (let* ((ugly-return nil)
              (sep ";")
              (python-process (or (get-buffer-process (current-buffer))
-                                 ;XXX hack for .py buffers
+					;XXX hack for .py buffers
                                  (get-process py-which-bufname)))
              ;; XXX currently we go backwards to find the beginning of an
              ;; expression part; a more powerful approach in the future might be
@@ -340,19 +340,26 @@ in the current *Python* session."
              (completions nil)
              (completion-table nil)
              completion
-             (comint-output-filter-functions
-              (append comint-output-filter-functions
-                      '(ansi-color-filter-apply
-                        (lambda (string)
-                                        ;(message (format "DEBUG filtering: %s" string))
-                          (setq ugly-return (concat ugly-return string))
-                          (delete-region comint-last-output-start
-                                         (process-mark (get-buffer-process (current-buffer)))))))))
-                                        ;(message (format "#DEBUG pattern: '%s'" pattern))
+	     (color-filter (if (featurep 'xterm-color)
+			       'xterm-color-filter
+			     'ansi-color-filter-apply))
+	     (simplefn (lambda (string)
+			 ;;(message (format "DEBUG filtering: %s" string))
+			 (setq ugly-return (concat ugly-return string))
+			 (delete-region comint-last-output-start
+					(process-mark (get-buffer-process (current-buffer))))))
+	     (comint-preoutput-filter-functions
+	      (append comint-preoutput-filter-functions
+		      (if (memq color-filter comint-preoutput-filter-functions)
+			  (list simplefn)
+			(list color-filter simplefn)))))
+	;;(message (format "#DEBUG pattern: '%s'" pattern))
         (process-send-string python-process
-                              (format ipython-completion-command-string pattern))
+			     (format ipython-completion-command-string pattern))
         (accept-process-output python-process)
-                                        ;(message (format "DEBUG return: %s" ugly-return))
+	;;(message (format "DEBUG return: %s" ugly-return))
+	(setq ugly-return
+	      (replace-regexp-in-string ".*#PYTHON-MODE SILENT\n" "" ugly-return))
         (setq completions
               (split-string (substring ugly-return 0 (position ?\n ugly-return)) sep))
         (setq completion-table (loop for str in completions
@@ -376,49 +383,57 @@ in the current *Python* session."
 in the current *Python* session."
     (interactive)
     (let* ((ugly-return nil)
-           (sep ";")
-           (python-process (or (get-buffer-process (current-buffer))
+	   (sep ";")
+	   (python-process (or (get-buffer-process (current-buffer))
                                         ;XXX hack for .py buffers
-                               (get-process py-which-bufname)))
-           ;; XXX currently we go backwards to find the beginning of an
-           ;; expression part; a more powerful approach in the future might be
-           ;; to let ipython have the complete line, so that context can be used
-           ;; to do things like filename completion etc.
-           (beg (save-excursion (skip-chars-backward "a-z0-9A-Z_./" (point-at-bol))
-                                (point)))
-           (end (point))
-           (pattern (buffer-substring-no-properties beg end))
-           (completions nil)
-           (completion-table nil)
-           completion
-         (comint-preoutput-filter-functions
-          (append comint-preoutput-filter-functions
-                  '(ansi-color-filter-apply
-                    (lambda (string)
-                      (setq ugly-return (concat ugly-return string))
-                      "")))))
-      (process-send-string python-process
-                            (format ipython-completion-command-string pattern))
-      (accept-process-output python-process)
-      (setq completions
-            (split-string (substring ugly-return 0 (position ?\n ugly-return)) sep))
-                                        ;(message (format "DEBUG completions: %S" completions))
-      (setq completion-table (loop for str in completions
-                                   collect (list str nil)))
-      (setq completion (try-completion pattern completion-table))
-      (cond ((eq completion t))
-            ((null completion)
-             (message "Can't find completion for \"%s\"" pattern)
-             (ding))
-            ((not (string= pattern completion))
-             (delete-region beg end)
-             (insert completion))
-            (t
-             (message "Making completion list...")
-             (with-output-to-temp-buffer "*IPython Completions*"
-               (display-completion-list (all-completions pattern completion-table)))
-             (message "Making completion list...%s" "done")))))
-)
+			       (get-process py-which-bufname)))
+	   ;; XXX currently we go backwards to find the beginning of an
+	   ;; expression part; a more powerful approach in the future might be
+	   ;; to let ipython have the complete line, so that context can be used
+	   ;; to do things like filename completion etc.
+	   (beg (save-excursion (skip-chars-backward "a-z0-9A-Z_./" (point-at-bol))
+				(point)))
+	   (end (point))
+	   (pattern (buffer-substring-no-properties beg end))
+	   (completions nil)
+	   (completion-table nil)
+	   completion
+	   (color-filter (if (featurep 'xterm-color)
+			     'xterm-color-filter
+			   'ansi-color-filter-apply))
+	   (simplefn (lambda (string)
+		       (setq ugly-return (concat ugly-return string))
+		       ""))
+	   (comint-preoutput-filter-functions
+	    (append comint-preoutput-filter-functions
+		    (if (memq color-filter comint-preoutput-filter-functions)
+			(list simplefn)
+		      (list color-filter simplefn)))))
+      (unless (= (length pattern) 0)
+	(process-send-string python-process
+			     (format ipython-completion-command-string pattern))
+	(accept-process-output python-process)
+	(setq ugly-return
+	      (replace-regexp-in-string ".*#PYTHON-MODE SILENT\n" "" ugly-return))
+	(setq completions
+	      (split-string (substring ugly-return 0 (position ?\n ugly-return)) sep))
+	;;(message (format "DEBUG completions: %S" completions))
+	(setq completion-table (loop for str in completions
+				     collect (list str nil)))
+	(setq completion (try-completion pattern completion-table))
+	(cond ((eq completion t))
+	      ((null completion)
+	       (message "Can't find completion for \"%s\"" pattern)
+	       (ding))
+	      ((not (string= pattern completion))
+	       (delete-region beg end)
+	       (insert completion))
+	      (t
+	       (message "Making completion list...")
+	       (with-output-to-temp-buffer "*IPython Completions*"
+		 (display-completion-list (all-completions pattern completion-table)))
+	       (message "Making completion list...%s" "done"))))))
+  )
 
 ;;; autoindent support: patch sent in by Jin Liu <m.liu.jin@gmail.com>,
 ;;; originally written by doxgen@newsmth.net
